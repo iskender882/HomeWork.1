4c18efdaacb5cf8a6f04b73de91eae2561c1a997
Iskender882 <isken.tabyldiev@icloud.com>
2021-05-24 20:12:05 +0300

    Д/3

---
+++ src/com/geektech/Main.java
@@ -0,0 +1,15 @@
+package com.geektech;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String world = "World";
+        final String hello = "Hello";
+
+
+        System.out.println( hello + " " + world);
+
+
+    }
+
+}
